7d9390c91c21f6746a5fc0bc771c4d334188208c
ZacharyAkers <zakhariakers@gmail.com>
2020-11-12 11:04:46 -0500

    fixed

--- week01/3-Wednesday/labs/inputs.py
+++ week01/3-Wednesday/labs/inputs.py
@@ -1,10 +1,14 @@
 
 # Please enter your name: Eric
 # Hello, Eric!
-
+name = input('What is your name? ')
+print(f"Hello, {name}")
 
 # Prompt the user for his name using the input function. Example:
-
+name = input('What is your name? ')
+length_of_name = len(name)
+print(f"Hello, {name}".upper())
+print(f"Your name has {length_of_name} letters".upper())
 
 # Same as the first exercise, but this time print the user's
 # name in ALL CAPS, and also tell them the number of letters in their name.
